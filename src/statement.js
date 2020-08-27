d88f4c4a1ec3d3928eff085c4839587a0d5b930e
Teddy <1294911641@qq.com>
2020-08-27 14:58:28 +0800

    [Refactoring][Teddy] refactor: extract amount calculation to calculateAmount method

--- src/statement.js
+++ src/statement.js
@@ -1,50 +1,53 @@
 function usd(thisAmount) {
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
-  return format(thisAmount / 100);
+    const format = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+    }).format;
+    return format(thisAmount / 100);
 }
 
-function statement (invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
-
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+function calculateAmount(perf, play) {
     let thisAmount = 0;
     switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
+        case 'tragedy':
+            thisAmount = 40000;
+            if (perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+        case 'comedy':
+            thisAmount = 30000;
+            if (perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`unknown type: ${play.type}`);
     }
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    //print line for this order
-    result += ` ${play.name}: ${(usd(thisAmount))} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
-  }
-  result += `Amount owed is ${(usd(totalAmount))}\n`;
-  result += `You earned ${volumeCredits} credits \n`;
-  return result;
+    return thisAmount;
+}
+
+function statement(invoice, plays) {
+    let totalAmount = 0;
+    let volumeCredits = 0;
+    let result = `Statement for ${invoice.customer}\n`;
+    for (let perf of invoice.performances) {
+        const play = plays[perf.playID];
+        totalAmount += calculateAmount(perf, play);
+        // add volume credits
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        // add extra credit for every ten comedy attendees
+        if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        //print line for this order
+        result += ` ${play.name}: ${(usd(calculateAmount(perf, play)))} (${perf.audience} seats)\n`;
+    }
+    result += `Amount owed is ${(usd(totalAmount))}\n`;
+    result += `You earned ${volumeCredits} credits \n`;
+    return result;
 }
 
 module.exports = {
-  statement,
+    statement,
 };
